1276661528168f8f06db7b21cca7f6796195badb
Meir <mrzkeller@gmail.com>
2020-01-16 15:19:55 +0200

    Updates

---
+++ precache-manifest.952d7e6a4392a032775a9bf473da9ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6c1b518d4d5088b87d5e730a701807",
+    "url": "/achieved-home-task/index.html"
+  },
+  {
+    "revision": "074951300d4cb77472ff",
+    "url": "/achieved-home-task/static/js/2.7bb1bdcd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/achieved-home-task/static/js/2.7bb1bdcd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3342df5efef37fe916c",
+    "url": "/achieved-home-task/static/js/main.c68b1b6b.chunk.js"
+  },
+  {
+    "revision": "a3eaf285259c041cca7c",
+    "url": "/achieved-home-task/static/js/runtime-main.7cd246f2.js"
+  },
+  {
+    "revision": "9922af30af3f7bde3b8f7cd9f050c320",
+    "url": "/achieved-home-task/static/media/clock.9922af30.svg"
+  }
+]);
